07f4368503aeb3665462a8ce3f615fa983c1e7dc
Theneillsaaco <isaacdepena18@gmail.com>
2026-01-03 15:05:03 -0400

    Remove `AdminDepartamentos.App` project and associated files:
    - Deleted project files, Razor components, models, services, and related assets.
    - Cleaned up references and configurations, including launch settings and imported namespaces.

--- AdminDepartamentos.App/Models/AuthResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AdminDepartamentos.App.Models;
-
-public class AuthResponse
-{
-    public bool IsAuthenticated { get; set; }
-    
-    public string User { get; set; }
-}
--- AdminDepartamentos.App/Models/InquilinoModels/InquilinoSaveModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AdminDepartamentos.App.Models.InquilinoModels;
-
-public class InquilinoSaveModel
-{
-    public InquilinoDto InquilinoDto { get; set; }
-
-    public PagoDto PagoDto { get; set; }
-}
